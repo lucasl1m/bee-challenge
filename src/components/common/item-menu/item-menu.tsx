b1d7a1d423036327ed060cf5be2e90a4a9ee0abe
Lucas Araújo de Lima <70279700+lucasl1m@users.noreply.github.com>
2023-03-09 08:12:43 -0300

    refactor: ItemMenu component to follow kebab-case

--- src/components/common/item-menu/item-menu.tsx
+++ src/components/common/item-menu/item-menu.tsx
@@ -1,10 +1,10 @@
 import { NavLink } from 'react-router-dom';
 
-import { usePath } from '../hooks/usePath';
+import { usePath } from '../../../hooks/use-path';
 
 import clsx from 'clsx';
 
-interface ItemMenuProps {
+export interface ItemMenuProps {
     path: string;
     name: string;
 }
@@ -15,7 +15,7 @@ export function ItemMenu({ path, name }: ItemMenuProps): JSX.Element {
     return (
         <NavLink
             to={path}
-            className={clsx('flex items-center px-4 py-2 text-sm font-semibold rounded hover:text-gray-800', {
+            className={clsx('flex items-center px-4 py-2 text-sm font-semibold rounded hover:text-gray-800 w-max', {
                 'bg-neutral-200 text-gray-800': isActive,
                 'text-gray-500 hover:bg-neutral-200': !isActive,
             })}
