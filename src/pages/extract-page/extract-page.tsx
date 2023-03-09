6dafb94c2631b0bffa5c359c29054be92f1e71f9
Lucas Araújo de Lima <70279700+lucasl1m@users.noreply.github.com>
2023-03-09 08:17:24 -0300

    refactor: Extract page to follow kebab-case

--- src/pages/extract-page/extract-page.tsx
+++ src/pages/extract-page/extract-page.tsx
@@ -1,6 +1,6 @@
 export function Extract(): JSX.Element {
     return (
-        <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col my-4 gap-4 lg:my-6">
             <h1>Extrato</h1>
         </div>
     );
