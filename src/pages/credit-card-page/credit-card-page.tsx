18b57bf444fe7776b5dc62b4f2ac2cc820bc13f8
Lucas Araújo de Lima <70279700+lucasl1m@users.noreply.github.com>
2023-03-09 08:17:10 -0300

    refactor: Credit Card page to follow kebab-case

--- src/pages/credit-card-page/credit-card-page.tsx
+++ src/pages/credit-card-page/credit-card-page.tsx
@@ -1,6 +1,6 @@
 export function CreditCard(): JSX.Element {
     return (
-        <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col my-4 gap-4 lg:my-6">
             <h1>Cartão de crédito</h1>
         </div>
     );
