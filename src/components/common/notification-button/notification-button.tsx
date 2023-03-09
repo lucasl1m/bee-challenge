387a1e9d746982b35e98c6a51f232ea3c1712400
Lucas Araújo de Lima <70279700+lucasl1m@users.noreply.github.com>
2023-03-09 08:09:28 -0300

    refactor: Notification to follow kebab-case

--- src/components/common/notification-button/notification-button.tsx
+++ src/components/common/notification-button/notification-button.tsx
@@ -6,8 +6,8 @@ export function Notifications(): JSX.Element {
     return (
         <Popover.Root>
             <Popover.Trigger>
-                <button className="flex items-center justify-center p-2 rounded-pill bg-neutral-200 hover:bg-neutral-300 md:p-3 lg:p-4">
-                    <MdOutlineNotificationsNone className="text-xl text-neutral-800" />
+                <button className="flex items-center justify-center p-2 rounded-pill bg-neutral-200 hover:bg-neutral-300">
+                    <MdOutlineNotificationsNone className="text-lg text-neutral-800 md:text-xl" />
                 </button>
             </Popover.Trigger>
         </Popover.Root>
