bda8ddf220faba0e81e018cc1f676af9c6f2691e
Lucas Araújo de Lima <70279700+lucasl1m@users.noreply.github.com>
2023-03-09 08:11:30 -0300

    refactor: Pix page to follow kebab-case

--- src/pages/pix-page/pix-page.tsx
+++ src/pages/pix-page/pix-page.tsx
@@ -1,6 +1,6 @@
 export function Pix(): JSX.Element {
     return (
-        <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col my-4 gap-4 lg:my-6">
             <h1>Area Pix</h1>
         </div>
     );
